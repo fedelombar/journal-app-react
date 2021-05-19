b177607f5f95bc9547ecbee3462b7630a964b4f0
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-05-19 10:35:45 -0300

    first commit

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { RegisterScreen } from "../components/auth/RegisterScreen";
+
+export const AuthRouter = () => {
+  return (
+    <div className="auth__main">
+      <Switch>
+        <Route path="/auth/login" component={LoginScreen} />
+        <Route path="/auth/register" component={RegisterScreen} />
+
+        <Redirect to="/auth/login" />
+      </Switch>
+    </div>
+  );
+};
