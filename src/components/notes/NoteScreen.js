be008fc1bd233c17473c01436f3027e163fe2e24
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-05-28 17:03:30 -0300

    notes

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
   return (
     <div className="notes_main-content">
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+        />
+
+        <textarea
+          placeholder="What happened today"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://services.meteored.com/img/article/el-misterioso-rayo-verde-del-atardecer-218011-3_1280.jpg"
+            alt="imagen"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 };
