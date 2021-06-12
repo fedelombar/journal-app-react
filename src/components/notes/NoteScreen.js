9cc745e7b8e99d2c2991d4b36e396755f49dbfbb
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-11 22:41:25 -0300

    act

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { NotesAppBar } from "./NotesAppBar";
+import React from 'react';
+import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
   return (
-    <div className="notes_main-content">
+    <div className='notes_main-content'>
       <NotesAppBar />
 
-      <div className="notes__content">
+      <div className='notes__content'>
         <input
-          type="text"
-          placeholder="Some awesome title"
-          className="notes__title-input"
+          type='text'
+          placeholder='Some awesome title'
+          className='notes__title-input'
         />
 
         <textarea
-          placeholder="What happened today"
-          className="notes__textarea"
+          placeholder='What happened today'
+          className='notes__textarea'
         ></textarea>
 
-        <div className="notes__image">
+        <div className='notes__image'>
           <img
-            src="https://services.meteored.com/img/article/el-misterioso-rayo-verde-del-atardecer-218011-3_1280.jpg"
-            alt="imagen"
+            src='https://services.meteored.com/img/article/el-misterioso-rayo-verde-del-atardecer-218011-3_1280.jpg'
+            alt='imagen'
           ></img>
         </div>
       </div>
