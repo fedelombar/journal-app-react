8131461ead81a5246c5fb1fcfccd1772d0fe0b09
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-13 00:47:13 -0300

    form

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    email: "nando@gmail.com",
+    password: "123456",
+  });
+
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
 
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Email"
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
 
         <input
@@ -20,6 +35,8 @@ export const LoginScreen = () => {
           placeholder="Password"
           name="password"
           className="auth__input"
+          value={password}
+          onChange={handleInputChange}
         />
 
         <button type="submit" className="btn btn-primary btn-block">
