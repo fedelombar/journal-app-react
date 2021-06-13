d69f5b702872b0e5f45c3ac2ff9a0041e64f12bb
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-13 01:00:29 -0300

    firebase

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
+import { login } from "../../actions/auth";
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch();
+
   const [formValues, handleInputChange] = useForm({
     email: "nando@gmail.com",
     password: "123456",
@@ -12,7 +17,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    dispatch(login(12345, "Federico"));
   };
 
   return (
